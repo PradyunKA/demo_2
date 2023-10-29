90c9eb2be5f8e2977a9070085c9c92e1284fe5a7
Pradyun K A <pradyunka@Pradyuns-MacBook-Pro.local>
2023-10-28 20:54:05 -0700

    added comment

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  ); // next change
   runApp(const MyApp());
 }
 
